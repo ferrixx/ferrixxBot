0599c5bfe2c0cde8a2622f1f6e4a1347a7320441
Mert <mertali.oezel@gmail.com>
2021-01-09 23:48:31 +0100

    Code improvement / formatting, simplified if conditions and removed redundant variable assignment.

--- src/main/java/de/ferrixx/bot/listeners/MessageCoinListener.java
+++ src/main/java/de/ferrixx/bot/listeners/MessageCoinListener.java
@@ -2,9 +2,8 @@ package de.ferrixx.bot.listeners;/*
  * Copyright (c) 2021 ferrixxDE - Justin Ippen
  */
 
-import de.ferrixx.bot.settings.settings;
+import de.ferrixx.bot.settings.Settings;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -12,17 +11,11 @@ public class MessageCoinListener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent e) {
-        if(e.getMessageAuthor().isBotUser()) return;
-
-        String[] args = e.getMessageContent().split(" ");
-
-        Server server;
-        if(e.getServer().isPresent()) {
-            server = e.getServer().get();
-        } else {
-            e.getChannel().sendMessage(new EmbedBuilder().setColor(settings.embedcolorerror).setTitle("Bitte benutze Befehle auf einem Server Channel aus und nicht im Privaten Chat!"));
-            return;
+        if (!e.getMessageAuthor().isRegularUser()) return;
+        if (e.getServer().isEmpty()) {
+            e.getChannel().sendMessage(new EmbedBuilder()
+                    .setColor(Settings.embedcolorerror)
+                    .setTitle("Bitte benutze Befehle auf einem Server Channel aus und nicht im Privaten Chat!"));
         }
-
     }
 }
